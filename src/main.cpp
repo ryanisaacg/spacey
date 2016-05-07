dc43c6622361dc73d6b293dfa1aa7dbcfd700a05
Ryan Goldstein <ryanisaacg@gmail.com>
2016-05-07 18:01:00 -0400

    Added a function to convert a line to space-only indentation

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ Convert a line of text from spaces or a mix to tab-only
 Useful for the guarantee that a line will be indented with tabs
 Pure function
 */
-std::string line_to_tabs(const std::string input, const unsigned int spaces_per) {
+const std::string line_to_tabs(const std::string input, const unsigned int spaces_per) {
 	std::string new_str = "";
 	unsigned int running_spaces = 0;
 	bool finished_whitespace = false;
@@ -41,7 +41,25 @@ std::string line_to_tabs(const std::string input, const unsigned int spaces_per)
 	return new_str;
 }
 
+const std::string line_to_spaces(const std::string input, const unsigned int spaces_per) {
+	const std::string tabs_only = line_to_tabs(input, spaces_per); //Tabs for consistency
+	std::string new_str = "";
+	std::string tabbed_size = "";
+	unsigned int i;
+	for(i = 0; i < spaces_per; i++)
+		tabbed_size += " ";
+	for(i = 0; i < tabs_only.length(); i++)
+	{
+		if(tabs_only.at(i) == '\t')
+			new_str += tabbed_size;
+		else
+			break;
+	}
+	new_str += tabs_only.substr(i);
+	return new_str;
+}
+
 int main() {
-	std::cout << line_to_tabs("  \ta  ", 4);
+	std::cout << line_to_spaces(line_to_tabs("\t\ta", 4), 4);
 	return 0;
 }
