258b15b29c2f436b7abdec41acdc717fa248b59a
Ryan Goldstein <ryanisaacg@gmail.com>
2016-05-08 06:52:31 -0400

    Refactored functions out of the main cpp file

---
+++ src/spacey.h
@@ -0,0 +1,14 @@
+#ifndef SPACEY_H_
+#define SPACEY_H_
+#include <string>
+
+/*
+Convert a line of text from spaces or a mix to tab-only
+Useful for the guarantee that a line will be indented with tabs
+Pure function
+*/
+const std::string line_to_tabs(const std::string input, const unsigned int spaces_per);
+
+const std::string line_to_spaces(const std::string input, const unsigned int spaces_per);
+
+#endif
